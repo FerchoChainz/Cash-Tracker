00ddea872346733e60202aa098aae5dd7e07fa9f
FerchoChainz <a21110132@ceti.mx>
2026-07-31 16:56:52 -0600

    Changing .env APP_NAME

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,6 +3,7 @@
 @section('contents')
 
     <main class="max-w-2xl mt-10 mx-auto p-10 shadow-lg">
+        <h1 class="font-black text-4xl">@yield('title')</h1>
         @yield('auth-contents')
     </main>
 
