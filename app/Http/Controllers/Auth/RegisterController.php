9b2df538357db7b8a8b90eb45ea6d9f6ac1009cd
FerchoChainz <a21110132@ceti.mx>
2026-08-05 19:15:22 -0600

    First endpoints GET to views

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,4 +10,13 @@ class RegisterController extends Controller
     public function index(){
         return view('auth.register');
     }
+
+    public function store(Request $request){
+        // Handle the registration logic here
+        $name = $request->input('name');
+        $email = $request->input('email');
+
+
+        return "Hola: $name, tu email es: $email";
+    }
 }
